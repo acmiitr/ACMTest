2ca6ab5c28dd92919ce957833fdf460d24b5d1bf
Rsingh2805 <rs280599@gmail.com>
2020-03-26 20:18:35 +0530

    Team revamp push

--- src/assets/team.js
+++ src/assets/team.js
@@ -1,94 +1,392 @@
 import aarush from './../assets/images/team/aarush.jpeg';
-// import amajaiswal from './../assets/images/team/amajaiswal.jpg';
-// import amanverma from './../assets/images/team/amanverma.jpg';
 import dakshit from './../assets/images/team/dakshit.jpeg';
 import patra from './../assets/images/team/patra.jpeg';
-// import chaubey from './../assets/images/team/chaubey.png';
-// import dikshachaudhary from './../assets/images/team/dikshachaudhary.jpg';
-// import ghanshyam from './../assets/images/team/ghanshyam.jpg';
-// import karansingh from './../assets/images/team/karansingh.jpeg';
-// import roodram from './../assets/images/team/roodram.jpg';
 import rahul from './../assets/images/team/rahul.jpeg';
 import akshat from './../assets/images/team/akshat.jpeg';
 import sachin from './../assets/images/team/sachin.jpeg';
 import aryaman from './../assets/images/team/aryaman.jpeg';
 import vipul from './../assets/images/team/vipul.jpg';
 
+import chaubey from './../assets/images/team/chaubey.png';
+import roodram from './../assets/images/team/roodram.jpg';
+import amanjaiswal from './../assets/images/team/aman-jaiswal.jpg';
+import amanverma from './../assets/images/team/aman-verma.jpeg';
+import aniketAggarwal from './../assets/images/team/aniket-agarwal.jpg';
+import gopiKishan from './../assets/images/team/gopi-kishan.jpeg';
+import mansi from './../assets/images/team/mansi.jpg';
+import siddhant from './../assets/images/team/siddhant.jpg';
+import ghetia from './../assets/images/team/ghetia.jpg';
+
+import anmolkhemka from './../assets/images/team/anmolkhem.jpeg';
+import dikshachaudhary from './../assets/images/team/dikshachaudhary.jpg';
+import masih from './../assets/images/team/masih.jpg';
+import karansingh from './../assets/images/team/karansingh.jpg';
+import priyanshagarwal from './../assets/images/team/priyanshagarwal.jpg';
+import rishi from './../assets/images/team/rishi.jpg';
+import suraaj from './../assets/images/team/suraajks.jpg';
+
+import gyanvaibhav from './../assets/images/team/gyanvaibhav.jpg';
+import pranaw from './../assets/images/team/pranavraj.jpg';
+import rahulagarwal from './../assets/images/team/rahulaggarwal.jpg';
+
+
 export const teamList = {
     bearers: [
         {
-            name: 'Dakshit Agrawal',
-            position: 'Treasurer',
-            github: 'dakshitagrawal97',
-            fb: 'neelagrawal08',
-            mail: 'dagrawal@cs.iitr.ac.in',
-            image: dakshit,
-            slack: 'dk'
-        },
-        {
-            name: 'Aarush Gupta',
-            position: 'Vice Chair',
-            github: 'aarushgupta',
-            fb: 'aarush.363',
-            mail: 'agupta1@cs.iitr.ac.in',
-            image: aarush,
-            slack: 'aarush'
-        },
-        {
-            name: 'Aniruddha Mahpatra',
-            position: 'Chair',
-            github: 'anime26398',
-            fb: 'aniruddha.mahapatra',
-            mail: 'amahapatra@cs.iitr.ac.in',
-            image: patra,
-            slack: 'animax'
-        }
+            "name": "Aniruddha Mahapatra",
+            "mail": "amahapatra@cs.iitr.ac.in",
+            "github": "anime26398",
+            "fb": "aniruddha.mahapatra",
+            "image": patra,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Aarush Gupta",
+            "mail": "agupta1@cs.iitr.ac.in",
+            "github": "aarushgupta",
+            "fb": "aarush.363",
+            "image": aarush,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Dakshit Agrawal",
+            "mail": "dagrawal@cs.iitr.ac.in",
+            "github": "dakshitagrawal97",
+            "fb": "neelagrawal08",
+            "image": dakshit,
+            "website": "https://dakshitagrawal97.github.io/",
+            "linkedin": "https://www.linkedin.com/in/dakshit97/",
+            "twitter": "@dakshit_97",
+            "position": ""
+          }
     ],
     heads: [
         {
-            name: 'Sachin Jain',
-            position: 'Operating System Discussion Head',
-            fb: '',
-            mail: 'sachinjain3677@gmail.com',
-            github: 'sachinjain3677',
-            image: sachin,
-            slack: 'sachin'
-        },
-        {
-            name: 'Vipul Jain',
-            position: 'Linux & Networks Discussion Head',
-            fb: 'vipul.ja1n',
-            mail: 'vjain@cs.iitr.ac.in',
-            github: 'VipulJai',
-            image: vipul,
-            slack: 'vipul'
-        },
-        {
-            name: 'Aryaman Maheshwari',
-            position: 'Events Manager',
-            fb: 'aryaman.maheshwari.7',
-            mail: 'amaheshwari1@cs.iitr.ac.in',
-            github: 'vaham-7',
-            image: aryaman,
-            slack: 'vaham7'
-        },
-        {
-            name: 'Rahul Singh',
-            position: 'Web Dev Lead',
-            fb: 'rahul.singh.280599',
-            mail: 'sachinjain3677@gmail.com',
-            github: 'Rsingh2805',
-            image: rahul,
-            slack: 'r_sin'
-        },
-        {
-            name: 'Akshat Arora',
-            position: 'Backend Head',
-            github: 'AkshatArora',
-            fb: 'akshataroraarora.arora',
-            mail: 'aarora@cs.iitr.ac.in',
-            image: akshat,
-            slack: 'akshat'
-        }
+            "name": "Akshat Arora",
+            "mail": "aarora@cs.iitr.ac.in",
+            "github": "AkshatArora",
+            "fb": "akshataroraarora.arora",
+            "image": akshat,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Aryaman Maheshwari",
+            "mail": "amaheshwari1@cs.iitr.ac.in",
+            "github": "vaham-7",
+            "fb": "aryaman.maheshwari.7",
+            "image": aryaman,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Rahul Singh",
+            "mail": "rsingh@cs.iitr.ac.in",
+            "github": "Rsingh2805",
+            "fb": "rahul.singh.280599",
+            "image": rahul,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Sachin Jain",
+            "mail": "sachinjain3677@gmail.com",
+            "github": "sachinjain3677",
+            "fb": "",
+            "image": sachin,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Vipul Jain",
+            "mail": "vjain@cs.iitr.ac.in",
+            "github": "VipulJai",
+            "fb": "vipul.ja1n",
+            "image": vipul,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
     ],
+    members: [
+        {
+            "name": "Aaryan Jain",
+            "mail": "ajain@cs.iitr.ac.in",
+            "github": "aary0510",
+            "fb": "Aary0510",
+            "image": "",
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Aman Jaiswal",
+            "mail": "ajaiswal@cs.iitr.ac.in",
+            "github": "jaiswalaman",
+            "fb": "amanjaiswal815",
+            "image": amanjaiswal,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Aman Verma",
+            "mail": "averma@cs.iitr.ac.in",
+            "github": "amanverma7",
+            "fb": "aman08verma12",
+            "image": amanverma,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Aniket Agarwal",
+            "mail": "aagarwal@ma.iitr.ac.in",
+            "github": "aniket-agarwal1999",
+            "fb": "aniket.agarwal.9484941",
+            "image": aniketAggarwal,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Ashutosh Chaubey",
+            "mail": "achaubey@cs.iitr.ac.in",
+            "github": "ac-alpha",
+            "fb": "ashutosh.chaubey.original",
+            "image": chaubey,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Gaurav Dewat",
+            "mail": "gdewat@cs.iitr.ac.in",
+            "github": "gauravdewat",
+            "fb": "dewatgaurav",
+            "image": "",
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Gopi Kishan",
+            "mail": "gkishan@cs.iitr.ac.in",
+            "github": "GopiKishan14",
+            "fb": "gopi.kishan.17",
+            "image": gopiKishan,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Mansi Agarwal",
+            "mail": "magarwal@cs.iitr.ac.in",
+            "github": "Mansi449",
+            "fb": "MansiAgarwal449",
+            "image": mansi,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Roodram Paneri",
+            "mail": "rpaneri@cs.iitr.ac.in",
+            "github": "Roodram",
+            "fb": "roodram",
+            "image": roodram,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Siddhant Nayak",
+            "mail": "snayak@cs.iitr.ac.in",
+            "github": "toadchavay",
+            "fb": "siddhant.nayak.100",
+            "image": siddhant,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Ghetia Siddharth",
+            "mail": "gsiddharth@cs.iitr.ac.in",
+            "github": "gsid1",
+            "fb": "siddharth.ghetia.5",
+            "image": ghetia,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Vishal Garg",
+            "mail": "vgarg@cs.iitr.ac.in",
+            "github": "Vishalgarg425",
+            "fb": "",
+            "image": "",
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Anmol Khemuka",
+            "mail": "akhemuka@cs.iitr.ac.in",
+            "github": "anmolkhemuka",
+            "fb": "Anmolkhemuka8",
+            "image": anmolkhemka,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Diksha Chaudhary",
+            "mail": "diksha@cs.iitr.ac.in",
+            "github": "diksha628",
+            "fb": "diksha.chaudhary.986227",
+            "image": dikshachaudhary,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Karan Singh",
+            "mail": "ksingh1@cs.iitr.ac.in",
+            "github": "KaranKS",
+            "fb": "karan976",
+            "image": karansingh,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Masih Ahmed",
+            "mail": "mahmed@cs.iitr.ac.in",
+            "github": "anon766",
+            "fb": "masih.ahmed.3",
+            "image": masih,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Priyansh Agarwal",
+            "mail": "pagarwal@cs.iitr.ac.in",
+            "github": "priyanshagarwal18",
+            "fb": "priyansh.agarwal.7",
+            "image": priyanshagarwal,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Priyanshu Garg",
+            "mail": "pgarg1@cs.iitr.ac.in",
+            "github": "priyanshu219",
+            "fb": "priyanshugarg",
+            "image": "",
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Rishi Chordia",
+            "mail": "rchordia@cs.iitr.ac.in",
+            "github": "rishichordia",
+            "fb": "rishi.chordia.5",
+            "image": rishi,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Suraaj K S",
+            "mail": "skanniwadi@cs.iitr.ac.in",
+            "github": "kssuraaj28",
+            "fb": "",
+            "image": suraaj,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Ghanshyam Singh Moyal",
+            "mail": "ghanshyam_s@me.iitr.ac",
+            "github": "jarvis-001",
+            "fb": "ghanshyam singh moyal",
+            "image": "",
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Gyan Vaibhav",
+            "mail": "gyan_v@ec.iitr.ac.in",
+            "github": "AntiEXE",
+            "fb": "gyan.vaibhav",
+            "image": gyanvaibhav,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Pranaw Raj",
+            "mail": "pranaw_r@cs.iitr.ac.in",
+            "github": "pranaw154",
+            "fb": "pranaw.raj.509",
+            "image": pranaw,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          },
+          {
+            "name": "Rahul Agrawal",
+            "mail": "rahul_a@cs.iitr.ac.in",
+            "github": "rahulag2411",
+            "fb": "rahul.raga.2411",
+            "image": rahulagarwal,
+            "website": "",
+            "linkedin": "",
+            "twitter": "",
+            "position": ""
+          }
+    ]
 }
--- src/components/TeamComponent/index.js
+++ src/components/TeamComponent/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import {Container, Row, Col, Button, ButtonGroup} from 'react-bootstrap';
 import TeamImage from './../../assets/images/team/full-team.jpg';
 import {teamList} from './../../assets/team';
-import './index.scss'
+import './index.scss';
+import './card.scss';
+
+
+const getRandomColorString = () => {
+    return `${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)}`;
+}
+
+const getTextColorString = (color) => {
+    const rgb = color.split(',');
+    const brightness = Math.round(((parseInt(rgb[0]) * 299) +
+    (parseInt(rgb[1]) * 587) +
+    (parseInt(rgb[2]) * 114)) / 1000);
+    return (brightness > 125) ? '#181818' : 'white';
+}
 
 const BearerMemberCard = props => {
     return (
@@ -31,6 +45,53 @@ const BearerMemberCard = props => {
     )
 }
 
+const NewDemoCard = props => {
+    const overlay_color = getRandomColorString();
+    const alt_color = getTextColorString(overlay_color);
+    return (
+        <div className="card-container position-relative">
+            <div className="position-absolute card-new member-card" style={{backgroundImage: `linear-gradient(0deg, rgba(${overlay_color}, 0.2), rgba(${overlay_color}, 0.2)), url(${props.member.image})`}}>
+                
+            </div>
+            <div class="position-absolute overlay px-2 d-flex flex-column" style={{backgroundColor: `rgba(${overlay_color}, 0.6)`, color: `${alt_color}`}}>
+                <div class="inner">
+                    <div className="member-card-name">{props.member.name}</div>
+                    <div className="member-card-position">{props.member.position}</div>
+                    <div className="member-card-social-icons">                        
+                        {
+                            props.member.fb?(
+                            <Button style={{color: `rgba(${overlay_color}, 0.6)`, backgroundColor: `${alt_color}`}} variant={"light"} className={"member-card-social-icon m-1"} 
+                            href={`https://facebook.com/${props.member.fb}`} target="blank">
+                            <i className="fa fa-facebook-square"></i>
+                            </Button>
+                            ):(<React.Fragment/>)
+                        }
+                        {
+                            props.member.github?(
+                            <Button style={{color: `rgba(${overlay_color}, 0.6)`, backgroundColor: `${alt_color}`}} variant={"light"} className={"member-card-social-icon m-1"} 
+                            href={`https://github.com/${props.member.github}`} target="blank">
+                            <i className="fa fa-github"></i>
+                            </Button>
+                            ):(<React.Fragment/>)
+                            
+                        }
+                        {
+                            props.member.mail?(
+                            <Button style={{color: `rgba(${overlay_color}, 0.6)`, backgroundColor: `${alt_color}`}} variant={"light"} className={"member-card-social-icon m-1"} 
+                            href={`mailto:${props.member.mail}`} target="blank">
+                            <i className="fa fa-envelope"></i>
+                            </Button>
+                            ):(<React.Fragment/>)
+                        }
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
 export const TeamComponent = props => {
     return (
         <React.Fragment>
@@ -42,10 +103,11 @@ export const TeamComponent = props => {
             <h3 className="text-center">Office Bearers</h3>
             <Row className={'justify-content-md-center'}>
                 {
-                    teamList['bearers'].map((obj, i) => {
+                    teamList['bearers'].map((obj, i) => { 
                         return (
-                            <Col key={i} xs={12} sm={6} lg={4} className="px-4 py-2">
-                                <BearerMemberCard member={obj} key={i}/>
+                            <Col key={i} xs={4} sm={3} lg={2} style={{padding: 0}} className="py-2">
+                                {/* <BearerMemberCard member={obj} key={i}/> */}
+                                <NewDemoCard member={obj}/>
                             </Col>
                         )
                     })
@@ -56,8 +118,22 @@ export const TeamComponent = props => {
                 {
                     teamList['heads'].map((obj, i) => {
                         return (
-                            <Col key={i} xs={12} sm={6} lg={4} className="px-4 py-2">
-                                <BearerMemberCard member={obj} key={i}/>
+                            <Col key={i} xs={4} sm={3} lg={2} style={{padding: 0}} className="py-2">
+                                {/* <BearerMemberCard member={obj} key={i}/> */}
+                                <NewDemoCard member={obj}/>
+                            </Col>
+                        )
+                    })
+                }
+            </Row>
+            <h3 className="mt-4 text-center">Members</h3>
+            <Row className={"justify-content-md-center mb-4"}>
+                {
+                    teamList['members'].map((obj, i) => {
+                        return (
+                            <Col key={i} xs={4} sm={3} lg={2} style={{padding: 0}} className="">
+                                {/* <BearerMemberCard member={obj} key={i}/> */}
+                                <NewDemoCard member={obj}/>
                             </Col>
                         )
                     })
